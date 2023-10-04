1eaafba9e00644499de739838b0a3e7cf7ad89dd
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-05 04:24:02 +0530

    Added latest articles component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
 import Highlights from "./Highlights";
+import LatestArticles from "./LatestArticles";
 import Topics from "./Topics";
+import { ArticlesContext } from "./MainContainer";
+// import articles from "../api/articles.json";
 
 const data = [
   {
@@ -49,11 +53,35 @@ const data = [
   },
 ];
 
+const highlightsData = [{ id: 1 }];
+
 const HomePage = () => {
+  const articlesData = useContext(ArticlesContext);
+
   return (
     <div>
-      <Highlights />
+      <Highlights content={highlightsData} />
       <Topics heading="Topics" data={data} />
+      <LatestArticles
+        backgroundColor="#1F618D"
+        // backgroundColor="#138D75"
+        // backgroundColor="#2E4053"
+        heading="tech"
+        articles={articlesData}
+      />
+      <LatestArticles
+        // color="#1F618D"
+        // backgroundColor="#138D75"
+        heading="travel"
+        articles={articlesData}
+      />
+      <LatestArticles
+        // backgroundColor="#935116"
+        // backgroundColor="#138D75"
+        // backgroundColor={"#FA8072"}
+        heading="finance"
+        articles={articlesData}
+      />
     </div>
   );
 };
