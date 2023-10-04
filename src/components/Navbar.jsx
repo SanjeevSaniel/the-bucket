460cbbef4d4917abbf6add07c3c961bcc79e56ad
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-04 12:03:20 +0530

    Navbar beautification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
 
-const menuItems = ["Home", "Articles", "About"];
+const menuItems = ["home", "blog", "about"];
 
 const Navbar = () => {
-  const [currentMenu, setCurrentMenu] = useState("Home");
+  const [currentMenu, setCurrentMenu] = useState("home");
 
   const handleMenu = (menu) => {
     setCurrentMenu(menu);
   };
 
   return (
-    <div className="flex flex-row justify-between items-center p-7">
+    <div className="flex flex-row justify-between items-center px-14 py-7 mb-4">
       <div className="flex-initial">the bucket</div>
-      <div className="flex-initial bg-black rounded-lg">
+      <div className="flex-initial bg-[#292a43] rounded-lg">
         <ul role="list" className="flex flex-row justify-between">
           {menuItems.map((menu, index) => {
             return (
               <li
                 key={index}
-                className={`mx-2 p-2 hover:text-yellow-300 ${
-                  currentMenu === menu && "underline underline-offset-1"
+                className={`mx-2 p-2 text-white capitalize hover:text-yellow-300 hover:cursor-pointer ${
+                  currentMenu.toLowerCase() === menu.toLowerCase() &&
+                  "text-yellow-300 underline underline-offset-1"
                 }`}
                 onClick={() => handleMenu(menu)}
               >
