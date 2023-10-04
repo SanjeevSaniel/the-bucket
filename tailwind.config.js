b02dffe61e0ebd7a180fb3a786656bd83db66bdb
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-04 12:04:27 +0530

    configured box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        xl: "0 0px 20px 0px #2C2C2C",
+      },
+    },
   },
   plugins: [],
 };
