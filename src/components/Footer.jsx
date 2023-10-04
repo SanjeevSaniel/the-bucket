9a036635d687155070a52f1a7cfa82a76bf764ab
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-05 00:07:14 +0530

    Added footer content

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="text-center p-4">
+      <p>
+        the bucket | Made with &#x1F49B; &copy; {new Date().getUTCFullYear()}
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
