c5cb39a674adf11e6d3a00b63190bd9d53571375
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-05 00:08:36 +0530

    Added homepage and footer component

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,9 +1,13 @@
+import Footer from "./Footer";
+import HomePage from "./HomePage";
 import Navbar from "./Navbar";
 
 const MainContainer = () => {
   return (
     <div>
       <Navbar />
+      <HomePage />
+      <Footer />
     </div>
   );
 };
