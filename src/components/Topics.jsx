b45ecb34f6baf763d8cb0d0f35bc80d61f7a8910
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-05 00:07:46 +0530

    Added breakpoints

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 
 const Topics = ({ heading, data }) => {
   return (
-    <div className="bg-[#41448d] p-14 w-full text-white">
+    <div className="bg-[#41448d] p-5 lg:p-14 w-full text-white">
       <h1 className="capitalize underline underline-offset-4 mb-8 text-2xl">
         {heading}
       </h1>
-      <div className="flex flex-row flex-wrap justify-between my-4 gap-6">
+      <div className="flex flex-row flex-wrap justify-center items:center lg:justify-between lg:items-center my-4 gap-6">
         {data.map((d) => {
           return (
             <div key={d.id}>
