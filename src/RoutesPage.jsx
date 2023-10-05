aebd9ef1bf86b4bb8714b2bad224a102fe6e5bba
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-06 00:18:47 +0530

    Configured new route

--- src/RoutesPage.jsx
+++ src/RoutesPage.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import ArticlesPage from "./components/ArticlesPage";
 import MainContainer from "./components/MainContainer";
+import TopicPage from "./components/TopicPage";
 
 const RoutesPage = () => {
   return (
@@ -8,6 +9,7 @@ const RoutesPage = () => {
       <Routes>
         <Route path="/">
           <Route index element={<MainContainer />} />
+          <Route path="/category/:topic" element={<TopicPage />} />
           <Route path="/articles" element={<ArticlesPage />} />
         </Route>
       </Routes>
