5296109f9e2e17c0018207cd3cbd73ad4acea0e0
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-06 00:39:01 +0530

    Configured new post route

--- src/RoutesPage.jsx
+++ src/RoutesPage.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import ArticlesPage from "./components/ArticlesPage";
 import MainContainer from "./components/MainContainer";
 import TopicPage from "./components/TopicPage";
+import PostInDetail from "./components/PostInDetail";
 
 const RoutesPage = () => {
   return (
@@ -9,6 +10,7 @@ const RoutesPage = () => {
       <Routes>
         <Route path="/">
           <Route index element={<MainContainer />} />
+          <Route path="/category/:topic/:title" element={<PostInDetail />} />
           <Route path="/category/:topic" element={<TopicPage />} />
           <Route path="/articles" element={<ArticlesPage />} />
         </Route>
