efecd538412b52c29ddc380b8f2244c0570dfb3c
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-05 18:16:53 +0530

    Code cleanup

--- src/RoutesPage.jsx
+++ src/RoutesPage.jsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import ArticlesPage from "./components/ArticlesPage";
 import MainContainer from "./components/MainContainer";
-import Navbar from "./components/Navbar";
 
 const RoutesPage = () => {
   return (
     <>
-      {/* <Navbar /> */}
       <Routes>
         <Route path="/">
           <Route index element={<MainContainer />} />
