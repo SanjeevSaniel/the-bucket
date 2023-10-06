5d28935525d63e02ec08bfbac8d4a5277655c4c1
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-07 04:39:38 +0530

    Added social icons

--- src/components/PostInDetail.jsx
+++ src/components/PostInDetail.jsx
@@ -1,6 +1,13 @@
 import { useContext } from "react";
 import { ArticlesContext } from "./MainContainer";
 import { Link, useParams } from "react-router-dom";
+import Avatar from "react-avatar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faPinterest,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 const PostInDetail = () => {
   const articles = useContext(ArticlesContext);
@@ -41,9 +48,103 @@ const PostInDetail = () => {
               alt={article.caption}
               className="w-full max-h-[30rem] mt-5 mb-2 object-cover"
             />
-            <p>
-              {article.publisher}, {article.date}
-            </p>
+            <div className="flex justify-between items-center px-2">
+              <div className="flex items-center">
+                {article.publisher === "BucketListly Blog" && (
+                  <svg
+                    width="35"
+                    height="35"
+                    viewBox="0 0 48.57 48.57"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    className="inline-block h-10 w-10 mr-4 rounded-full ring-2 ring-white"
+                  >
+                    <title>BucketListly Logo</title>
+                    <path
+                      d="m48.56 24.28a24.28 24.28 0 1 0 -24.28 24.29 24.28 24.28 0 0 0 24.28-24.29z"
+                      fill="#eebf25"
+                    ></path>
+                    <path
+                      d="m12.506 19.144 1.258-.871 14.231 20.567-1.258.871zm20.844 1.626-7.78 5.38-1.29-1.87-3.32 2.3-6.22-8.99 5.91-4.09 2.24 3.24 7.54-5.21.61 5.21 4.66 2.4z"
+                      fill="#231f20"
+                    ></path>
+                  </svg>
+                )}
+                {article.publisher !== "BucketListly Blog" && (
+                  <span className="mr-2 ring-2 ring-white">
+                    <Avatar size="50px" round="100%" name={article.publisher} />
+                  </span>
+                  // <img
+                  //   className="inline-block h-10 w-10 mx-4 rounded-full ring-2 ring-white"
+                  //   src=""
+                  //   alt="A"
+                  // />
+                )}
+                <div>
+                  <p>{article.publisher}</p>
+                  <p>{article.date}</p>
+                </div>
+              </div>
+
+              {article.social?.length > 0 && (
+                <div className="text-right">
+                  <p className="text-xs uppercase font-sans pb-1">Connect</p>
+                  <div className="flex gap-x-2">
+                    {article?.social?.map((s) => {
+                      return (
+                        <a
+                          key={s.id}
+                          href={s.url}
+                          // className="social-circle instagram loaded"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          aria-label={s.arialabel}
+                        >
+                          {s.name === "instagram" && (
+                            <FontAwesomeIcon icon={faInstagram} size="lg" />
+                          )}
+
+                          {s.name === "youtube" && (
+                            <FontAwesomeIcon icon={faYoutube} size="lg" />
+                          )}
+                        </a>
+                      );
+                    })}
+
+                    {/* <a
+                    href="https://www.instagram.com/peachananr"
+                    className="social-circle instagram loaded"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Navigate to BucketListly's Instagram Page"
+                  >
+                    <FontAwesomeIcon icon={faInstagram} size="lg" />
+                  </a> */}
+
+                    {/* <a
+                    href="https://www.youtube.com/@bucketlistly?sub_confirmation=1"
+                    className="social-circle youtube loaded"
+                    rel="noreferrer"
+                    target="_blank"
+                    aria-label="Navigate to BucketListly's Youtube Page"
+                  >
+                    <FontAwesomeIcon icon={faYoutube} size="lg" />
+                  </a>
+
+                  <a
+                    href="https://www.pinterest.com/bucketlistly_blog"
+                    className="social-circle pinterest loaded"
+                    target="_blank"
+                    rel="noopener noreferrer external"
+                    aria-label="Navigate to BucketListly's Pinterest Page"
+                  >
+                    <FontAwesomeIcon icon={faPinterest} size="lg" />
+                  </a> */}
+                  </div>
+                </div>
+              )}
+            </div>
+
             <p className="my-5">{article.description}</p>
           </div>
 
@@ -115,10 +216,53 @@ const PostInDetail = () => {
               alt={article.caption}
               className="w-full max-h-[30rem] mt-5 mb-2 object-cover"
             />
-            <p>
+            {/* <p>
               {article.publisher}, {article.date}
             </p>
-            <p className="my-5">{article.description}</p>
+            <p className="my-5">{article.description}</p> */}
+
+            <div className="flex justify-between items-center p-2">
+              <div className="flex items-center">
+                {article.publisher === "BucketListly Blog" && (
+                  <svg
+                    width="35"
+                    height="35"
+                    viewBox="0 0 48.57 48.57"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    className="inline-block h-10 w-10 mr-4 rounded-full ring-2 ring-white"
+                  >
+                    <title>BucketListly Logo</title>
+                    <path
+                      d="m48.56 24.28a24.28 24.28 0 1 0 -24.28 24.29 24.28 24.28 0 0 0 24.28-24.29z"
+                      fill="#eebf25"
+                    ></path>
+                    <path
+                      d="m12.506 19.144 1.258-.871 14.231 20.567-1.258.871zm20.844 1.626-7.78 5.38-1.29-1.87-3.32 2.3-6.22-8.99 5.91-4.09 2.24 3.24 7.54-5.21.61 5.21 4.66 2.4z"
+                      fill="#231f20"
+                    ></path>
+                  </svg>
+                )}
+                {article.publisher !== "BucketListly Blog" && (
+                  <span className="mx-4 ring-2 ring-white">
+                    <Avatar size="50px" round="100%" name={article.publisher} />
+                  </span>
+                  // <img
+                  //   className="inline-block h-10 w-10 mx-4 rounded-full ring-2 ring-white"
+                  //   src=""
+                  //   alt="A"
+                  // />
+                )}
+                <div>
+                  <p>{article.publisher}</p>
+                  <p>{article.date}</p>
+                </div>
+              </div>
+
+              <div>
+                <p className="text-sm uppercase font-sans">Connect</p>
+              </div>
+            </div>
           </div>
 
           <div className="col-span-1 mt-10">
