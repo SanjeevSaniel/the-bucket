40a495e8e320e1dee31a24863089a685147f12b3
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-10-11 01:04:10 +0530

    A single components import file

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import ArticlesPage from "./ArticlesPage";
+import Footer from "./Footer";
+import GotoTop from "./GotoTop";
+import Navbar from "./Navbar";
+import Highlights from "./Highlights";
+import HomePage from "./HomePage";
+import InfoCard from "./InfoCard";
+import LatestArticles from "./LatestArticles";
+import PostInDetail from "./PostInDetail";
+import SlideShow from "./SlideShow";
+import TopicPage from "./TopicPage";
+import Topics from "./Topics";
+import Form from "./Form";
+import HorizonalScrollButton from "./HorizonalScrollButton";
+import MainContainer from "./MainContainer";
+
+export {
+  ArticlesPage,
+  Footer,
+  Form,
+  GotoTop,
+  Highlights,
+  HomePage,
+  HorizonalScrollButton,
+  InfoCard,
+  LatestArticles,
+  MainContainer,
+  Navbar,
+  PostInDetail,
+  SlideShow,
+  Topics,
+  TopicPage,
+};
