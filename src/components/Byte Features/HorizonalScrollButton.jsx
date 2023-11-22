b2ab41012bf55170010d342f5cb32ea3f29a0d74
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-11-22 15:17:15 +0530

    UI changes

--- src/components/Byte Features/HorizonalScrollButton.jsx
+++ src/components/Byte Features/HorizonalScrollButton.jsx
@@ -8,7 +8,7 @@ const HorizonalScrollButton = ({
     <button
       type="button"
       onClick={handleClick}
-      className={`absolute z-20 top-[10rem] bottom-[10rem] ${position} bg-slate-500 p-3 flex items-center text-yellow-300 text-2xl ${isHidden} opacity-80`}
+      className={`absolute z-20 top-[10rem] bottom-[10rem] ${position} bg-slate-500 dark:bg-slate-300 p-3 flex items-center text-yellow-300 dark:text-yellow-900 text-2xl ${isHidden} opacity-80 dark:opacity-90`}
     >
       {children}
     </button>
